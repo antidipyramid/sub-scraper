77b648e8f16247a356a582dbd96365be667fa2e5
antidipyramid <mstj77@protonmail.com>
2021-08-25 21:54:58 -0400

    added downloadurl for tampermonkey update

--- main.user.js
+++ main.user.js
@@ -7,8 +7,8 @@
 // @match        https://github.com/antidipyramid/sub-scraper/blob/scrape-subscriptions/main.js
 // @icon         https://www.google.com/s2/favicons?domain=github.com
 // @require      https://cdn.jsdelivr.net/npm/ramda@0.25.0/dist/ramda.min.js
+// @downloadURL  https://github.com/antidipyramid/sub-scraper/raw/scrape-subscriptions/main.user.js
 // @grant        GM_registerMenuCommand
-// @grant        GM.registerMenuCommand
 // ==/UserScript==
 
 // Driver for the sub-sraper. Should be run in a browser extension
