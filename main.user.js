a3ae6441b740614bda93081c6149b991da736adb
antidipyramid <mstj77@protonmail.com>
2021-08-30 20:17:56 -0400

    Completely switched to using Ramda, created pipeline in main, and added download function

--- main.user.js
+++ main.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  try to take over the world!
-// @author       You
+// @author       antidipyramid
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?domain=github.com
 // @require      https://cdnjs.cloudflare.com/ajax/libs/ramda/0.27.1/ramda.min.js
@@ -16,7 +16,8 @@
 // like Greasemonkey or Tampermonkey.
 
 const directChannelPrefix = "channel",
-  altChannelURLPrefixes = new Set(["c", "user"]);
+  altChannelURLPrefixes = new Set(["c", "user"]),
+  excludedPrefixes = new Set(["feed"]);
 
 /**
  * Fetches the channel ID given the URL of a YouTube channel.
@@ -77,6 +78,17 @@ async function fetchChannelID(channelURL) {
     });
 }
 
+/**
+ * Given the HTML element of a subscription from the YouTube sidebar,
+ * return the title and link of the channel.
+ *
+ * @param {HTMLElement} subscriptionHTMLElement - contains the relevant
+ * information about a subscription
+ *
+ * @return {Object} - an object containing the name of the channel
+ * and the link to the channel's page
+ *
+ */
 function getNamesAndLinks(subscriptionHTMLElement) {
   return {
     channelName: subscriptionHTMLElement.title,
@@ -84,29 +96,126 @@ function getNamesAndLinks(subscriptionHTMLElement) {
   };
 }
 
+/**
+ * Given an object with the channel's name and link, add the channel's ID.
+ *
+ * @param {Object} channelNameAndLink - an object with the channel's name and link
+ * @return {Object} - the input object with an added attribute for the channel ID
+ *
+ */
+function addChannelID(channelNameAndLink) {
+  return R.pipe(
+    R.prop("channelLink"),
+    getChannelID,
+    R.andThen(R.objOf("channelID"))
+  )(channelNameAndLink).then((channelID) => {
+    return R.mergeLeft(channelNameAndLink, channelID);
+  });
+}
+
+/**
+ * Given the channelID of a subscription, adds the link to it's RSS feed.
+ *
+ * @param {Object} channelInfo - an object containing the channel's info
+ * @return {string} - the input object with an added attribute for the channel's
+ * RSS link
+ */
+function addChannelRSSLink(channelInfo) {
+  return R.assoc(
+    "channelRSSLink",
+    "https://www.youtube.com/feeds/videos.xml?channel_id=" +
+      channelInfo.channelID
+  );
+}
+
+/**
+ * Given an object with the channel's info, add an attribute containing the
+ * XML element to be added to the OMPL file.
+ *
+ * @param {Object} channelInfo - an object containng the channel's info
+ * @return {Object} the input object with an added attribute for the XML element
+ *
+ */
+function addChannelXMLString(channelInfo) {
+  return R.assoc(
+    "channelXMLString",
+    "<outline xmlUrl='" - channelInfo.channelRSSLink + "'/>",
+    channelInfo
+  );
+}
+
+/**
+ * Generates an empty OPML document that the subscriptions will be added to.
+ *
+ * @return {XMLDocument} an empty OPML document
+ */
+function getEmptyOPMLDocument() {
+  const doc = document.implementation.createDocument(null, "opml", null);
+
+  const body = doc.createElement("body");
+
+  const outerOutline = doc.createElement("outline");
+  outerOutline.setAttribute("text", "My Youtube Subscriptions");
+  outerOutline.setAttribute("title", "My Youtube Subscriptions");
+  body.appendChild(outerOutline);
+
+  doc.documentElement.appendChild(body);
+
+  return doc;
+}
+
+/**
+ * Prompts the user to download the OPML file.
+ */
+function promptDownload(xmlDocument) {
+  var filename = "subs.xml";
+  var pom = document.createElement("a");
+  var blob = new Blob([new XMLSerializer().serializeToString(xmlDocument)], {
+    type: "text/plain",
+  });
+
+  pom.setAttribute("href", window.URL.createObjectURL(blob));
+  pom.setAttribute("download", filename);
+
+  pom.dataset.downloadurl = ["text/plain", pom.download, pom.href].join(":");
+  pom.draggable = true;
+  pom.classList.add("dragout");
+
+  pom.click();
+}
+
 function main() {
   const subscriptionHTMLElements = document.querySelectorAll(
     "div#sections ytd-guide-section-renderer:nth-child(2) a#endpoint[href]"
   );
 
-  const makeChannelIDObj = R.pipe(
-    R.prop("channelLink"),
-    getChannelID,
-    R.andThen(R.objOf("channelID"))
-  );
+  const addChild = (element, child) => {
+    element.appendChild(child);
+    return element;
+  };
 
-  const channelNamesAndLinks = R.map(
-      getNamesAndLinks,
-      subscriptionHTMLElements
-    ),
-    channelIDList = R.map(makeChannelIDObj, channelNamesAndLinks);
+  R.pipe(
+    R.map(getNamesAndLinks),
+    R.map(addChannelID),
+    R.bind(Promise.all, Promise)
+  )(Array.from(subscriptionHTMLElements)).then((channelInfo) => {
+    const parser = new DOMParser(),
+      convertStringToXML = R.curry(R.bind(parser.parseFromString, parser)),
+      doc = getEmptyOPMLDocument(),
+      root = doc.querySelector("outline"),
+      addSubscriptionlToXMLDoc = R.curry(addChild)(root);
 
-  Promise.all(channelIDList).then((channelIDs) => {
     R.pipe(
-      R.zip(channelNamesAndLinks),
-      R.map(R.mergeAll),
-      console.log
-    )(channelIDs);
+      R.map(addChannelRSSLink),
+      R.map(addChannelXMLString),
+      R.map(R.prop("channelXMLString")),
+      R.map(convertStringToXML(R.__, "text/xml")),
+      R.map((d) => d.querySelector("outline")),
+      R.map((n) => doc.adoptNode(n)),
+      R.forEach(addSubscriptionlToXMLDoc)
+    )(channelInfo);
+
+    promptDownload(doc);
   });
 }
 
